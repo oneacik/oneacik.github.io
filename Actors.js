f2b6a12c169083bf308f4f62267ae5662d9bfabc
psuwala <psuwala@atlassian.com>
2018-12-27 00:45:27 +0100

    changed exploding meteor hp to 0

--- Actors.js
+++ Actors.js
@@ -46,6 +46,7 @@ class Meteor extends BaseObject {
 class ExplodingMeteor extends Meteor {
     constructor(x,y, rotate){
         super(x,y,rotate);
+        this.hp = 0;
         this.setAllColors("#ff8d2d");
     }
 }
